beb94bfd1789335c5ac8ee469d3dd0a8d9808260
tushar-2811 <tusharrawat52@gmail.com>
2025-10-07 11:02:06 +0530

    fix gitignore

---
+++ services/user_service/src/middleware/authMiddleware.ts
@@ -0,0 +1,38 @@
+import type { NextFunction, Request, Response } from "express";
+import type { IUser } from "../types/index.js";
+import jwt, { type JwtPayload } from 'jsonwebtoken'
+
+export interface AuthenticatedRequest extends Request {
+    user?: IUser | null;
+}
+
+export const isAuthMiddleware = async(req: AuthenticatedRequest , res: Response , next: NextFunction):Promise<void> => {
+    try {
+        const authHeader = req.headers?.authorization;
+
+        if(!authHeader || !authHeader.startsWith("Bearer ")){
+           res.status(401).json({
+            success : false,
+            message : "Please login - No auth headers"
+           });
+           return;
+        }
+
+        const token = authHeader.split(" ")[1] || "";
+        const decodedValue = jwt.verify(token , process.env.JWT_SECRET as string) as JwtPayload;
+
+        if(!decodedValue || !decodedValue.user){
+            res.status(401).json({
+                success : false,
+                message : "Invalid Token"
+            });
+            return;
+        }
+
+        
+
+    } catch (error) {
+        
+    }
+
+}
